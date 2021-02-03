4d820553bc583c983105a574caa371aa6446c054
joaquin fox <joaquin.fox@gmail.com>
2021-02-03 13:37:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const ulEl = document.querySelector('ul');
 let showDescription = false;
 const data = [
   {
+  link:'./number_guessing_game/index.html',
   title:'number guessing game',
   desc: 'A user guesses at a randomly generated number, and game logic evaluates user responses and provides user feedback.',
   },
   {
+  link:'./color_picker_game/index.html',
   title:'color picker game',
   desc:'This game uses random number generation and packages these as an RGB text string to arrive at a random color pattern. User is presented with a color pallete and instructed to click the color that matches a target color.',
   },
   {
+  link:'https://joaquinfox.github.io/mdn-local-storage-demo/',
   title:'Local Storage API Demo',
   desc:'A demonstration of local storage. User inputs for font and color schemes are stored in local storage and retrieved on page refresh. The documentation provides a discussion of session storage mechanics.',
   },
@@ -20,9 +23,9 @@ const data = [
 data.forEach(i=>{
   let liEl = document.createElement('li');
   
-  liEl.innerHTML =` <h4>${i.title}</h4>
+  liEl.innerHTML =` <a href=${i.link}><h4>${i.title}</h4></a>
   <p class='hidePara'>${i.desc}</p>
-  <button class='showHide'> read more </button>`;
+  <button class='showHide'> ...read more </button>`;
 ulEl.appendChild(liEl);
 });
 
@@ -32,9 +35,9 @@ showHideBtns.forEach(button=>{
   button.addEventListener('click',()=>{
     (button.previousElementSibling.classList.toggle('hidePara'));
     if(button.previousElementSibling.getAttribute('class')){
-       button.textContent = 'read more';
+       button.textContent = '...read more';
     }else{
-           button.textContent='read less';
+           button.textContent='...read less';
 
     }
   });
