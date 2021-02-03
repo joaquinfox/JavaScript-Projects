3da4980b982898bcbc23d3b3b82133d969db0e17
joaquin fox <joaquin.fox@gmail.com>
2021-02-03 13:17:22 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,27 +1,41 @@
-// Reveal a substring of project descriptors with an option to reveal
-// capture the p elements
 const pElements = document.querySelectorAll('p');
-const pContent = [];
+const ulEl = document.querySelector('ul');
 let showDescription = false;
+const data = [
+  {
+  title:'number guessing game',
+  desc: 'A user guesses at a randomly generated number, and game logic evaluates user responses and provides user feedback.',
+  },
+  {
+  title:'color picker game',
+  desc:'This game uses random number generation and packages these as an RGB text string to arrive at a random color pattern. User is presented with a color pallete and instructed to click the color that matches a target color.',
+  },
+  {
+  title:'Local Storage API Demo',
+  desc:'A demonstration of local storage. User inputs for font and color schemes are stored in local storage and retrieved on page refresh. The documentation provides a discussion of session storage mechanics.',
+  },
+  ];
+  
+// Render data
+data.forEach(i=>{
+  let liEl = document.createElement('li');
+  
+  liEl.innerHTML =` <h4>${i.title}</h4>
+  <p class='hidePara'>${i.desc}</p>
+  <button class='showHide'> read more </button>`;
+ulEl.appendChild(liEl);
+});
 
-setText();
-function setText(){
-for(let i = 0; i < pElements.length; i++){
-  let content = pElements[i].textContent;
-  pElements[i].innerHTML = showDescription ?`${content}  <button class='readMore'> ...read less </button>`:`${content.substring(0,50)}  <button class='readMore'> ...read more </button>`;
-}}
+// Capture buttons
+let showHideBtns = document.querySelectorAll('.showHide');
+showHideBtns.forEach(button=>{
+  button.addEventListener('click',()=>{
+    (button.previousElementSibling.classList.toggle('hidePara'));
+    if(button.previousElementSibling.getAttribute('class')){
+       button.textContent = 'read more';
+    }else{
+           button.textContent='read less';
 
-const buttonElements = document.querySelectorAll('.readMore');
-
-for(let i = 0; i < buttonElements.length; i++){
-  buttonElements[i].addEventListener('click', toggleDescriptionDisplay);
-}
-
-function toggleDescriptionDisplay(){
-  // let content = pContent.filter(i=>i.index === this.parentNode.key)
-  // this.parentNode.innerHTML = `${content}  <button class='readMore'> ...read less </button>`;
-  // console.log()
-  showDescription = !showDescription;
-  console.log(showDescription)
-  setText();
-}
+    }
+  });
+});
